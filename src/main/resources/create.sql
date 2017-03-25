a2a37f872853ab3206a7723907a5cac0bdeb978f
milan987 <milan.z.markovic@gmail.com>
2017-03-25 16:16:22 +0100

    Test dao

---
+++ src/main/resources/create.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `stock` (
+  `STOCK_ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `STOCK_CODE` varchar(10) NOT NULL,
+  `STOCK_NAME` varchar(20) NOT NULL,
+  PRIMARY KEY (`STOCK_ID`) USING BTREE,
+)
--- src/test/java/com/m2/TestDAO.java
+++ src/test/java/com/m2/TestDAO.java
@@ -8,8 +8,17 @@ import com.m2.dao.StockDaoHibernate;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.io.File;
+import java.sql.SQLException;
 import java.util.List;
+
+import org.h2.tools.RunScript;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.sql.Connection;
 
 /**
  * Created by Milan on 25-Mar-17.
@@ -19,7 +28,17 @@ public class TestDAO {
 
     @Before
     public void init() {
-        entityManagerFactory = Persistence.createEntityManagerFactory( "JavaHelps" );
+        entityManagerFactory = Persistence.createEntityManagerFactory( "Testing" );
+        @Override
+        public void execute(Connection connection) throws SQLException {
+            try {
+                File script = new File(getClass().getResource("/data.sql").getFile());
+                RunScript.execute(connection, new FileReader(script));
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException("could not initialize with script");
+            }
+        }
+    });
     }
 
     @After
@@ -37,7 +56,15 @@ public class TestDAO {
     }
 
     @Test
+    @Transactional
+    @Rollback
     public void testDao1(){
-        
+        StockDao sda = new StockDaoHibernate();
+        sda.create(6,"Pera1","199");
+        sda.create(7,"Jelka1","166");
+        List<Stock> lista = sda.ReadAll();
+        for (Stock s : lista){
+            System.out.println(s.toString());
+        }
     }
 }
